d494cb6ccd4b3934176c58101ad3a82fb45a66b8
4zu1Q <135081843+4zu1Q@users.noreply.github.com>
2024-05-16 18:53:38 +0900

    up

--- SwordTrial/Assets/YSFolder/Player.cs
+++ SwordTrial/Assets/YSFolder/Player.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    /*変数宣言*/
+    private Vector3 m_pos;  //プレイヤーの座標
+    private Vector3 m_move; //プレイヤーの移動
+
+
+
     // Start is called before the first frame update
     void Start()
     {
