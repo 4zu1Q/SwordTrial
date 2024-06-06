7f7e6fd797688d8aab1218edbe7d074841d48555
itukikunmeuu <135081677+itukikunmeuu@users.noreply.github.com>
2024-06-06 19:00:27 +0900

    Enemy
    Enemyの挙動関係
    当たり判定を先にしようとしたら重力がいい感じに働かずにバグっている

--- SwordTrial/Assets/controller/MoveEnemy.cs
+++ SwordTrial/Assets/controller/MoveEnemy.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class MoveEnemy : MonoBehaviour
 {
     GameObject PlayerObj;                    //プレイヤーのオブジェクトの取得準備
-    private float m_speed = 1.0f;            //敵のスピードを1にする
-    private float m_playerToDis = 10.0f;     //プレイヤーが入ってきた際に追いかける距離
+    private float m_speed = 0.005f;            //敵のスピードを1にする
+    private float m_playerToDis = 7.0f;     //プレイヤーが入ってきた際に追いかける距離
     private Vector3 m_startPos;
     // Start is called before the first frame update
     void Start()
@@ -19,7 +19,6 @@ public class MoveEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0.001f,0,0);
 
         //プレイヤーの位置から自分の位置を引いた数だけノーマライズする
         Vector3 dir = (PlayerObj.transform.position - this.transform.position).normalized;
@@ -29,22 +28,22 @@ public class MoveEnemy : MonoBehaviour
         if(mag < m_playerToDis)
         {
             //プレイヤーに向かって移動する
-            //Vector3 nowPos     = transform.position;
-            //dir.y              = 0;
-            //Vector3 move       = dir.normalized * m_speed;
-            //nowPos             = nowPos + move;
-            //transform.position = nowPos;
-            //正規化
-            //Vector3 a = (PlayerObj.transform.position - this.transform.position).normalized;
-
-            ////その方向に指定したスピード量で進む
-            //float vx = dir.x * speed;
-            //float vz = dir.z * speed;
+            Vector3 nowPos = transform.position;
+            dir.y = 0;
+            Vector3 move = dir.normalized * m_speed;
+            nowPos = nowPos + move;
+            transform.position = nowPos;
 
             //プレイヤーの方向を向く処理
             float rot = Mathf.Atan2(dir.x, dir.z);
             transform.rotation = Quaternion.AngleAxis(rot * Mathf.Rad2Deg, Vector3.up);
+
+
         }
+        else
+        {
+        }
+        
 
     }
 }
