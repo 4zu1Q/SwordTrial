66cdfa4ee10c7433a6daede6a92502641b5a1751
itukikunmeuu <135081677+itukikunmeuu@users.noreply.github.com>
2024-06-06 17:45:10 +0900

    up

--- SwordTrial/Assets/controller/MoveEnemy.cs
+++ SwordTrial/Assets/controller/MoveEnemy.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class MoveEnemy : MonoBehaviour
 {
-    GameObject PlayerObj;                      //プレイヤーのオブジェクトの取得準備
-    private float m_speed = 1.0f;      //敵のスピードを1にする
+    GameObject PlayerObj;                    //プレイヤーのオブジェクトの取得準備
+    private float m_speed = 1.0f;            //敵のスピードを1にする
     private float m_playerToDis = 10.0f;     //プレイヤーが入ってきた際に追いかける距離
     private Vector3 m_startPos;
     // Start is called before the first frame update
@@ -14,7 +14,6 @@ public class MoveEnemy : MonoBehaviour
     {
         PlayerObj = GameObject.FindWithTag("Player");   //プレイヤーのオブジェクトの取得
         m_startPos = transform.position;                //座標の初期設定
-
     }
 
     // Update is called once per frame
@@ -28,11 +27,17 @@ public class MoveEnemy : MonoBehaviour
         if(mag < m_playerToDis)
         {
             //プレイヤーに向かって移動する
-            Vector3 nowPos     = transform.position;
-            dir.y              = 0;
-            Vector3 move       = dir.normalized * m_speed;
-            nowPos             = nowPos + move;
-            transform.position = nowPos;
+            //Vector3 nowPos     = transform.position;
+            //dir.y              = 0;
+            //Vector3 move       = dir.normalized * m_speed;
+            //nowPos             = nowPos + move;
+            //transform.position = nowPos;
+            //正規化
+            //Vector3 a = (PlayerObj.transform.position - this.transform.position).normalized;
+
+            ////その方向に指定したスピード量で進む
+            //float vx = dir.x * speed;
+            //float vz = dir.z * speed;
 
             //プレイヤーの方向を向く処理
             float rot = Mathf.Atan2(dir.x, dir.z);
