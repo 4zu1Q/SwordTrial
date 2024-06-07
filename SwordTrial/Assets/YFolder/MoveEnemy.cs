754a0e2bb219c04b695d7c7e39a70ee85dc40615
itukikunmeuu <135081677+itukikunmeuu@users.noreply.github.com>
2024-06-07 16:48:37 +0900

    Enemyの移動
    移動に違和感があったためそれの修正、Enemyの移動に関してはこれで終了。

--- SwordTrial/Assets/YFolder/MoveEnemy.cs
+++ SwordTrial/Assets/YFolder/MoveEnemy.cs
@@ -2,47 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MoveEnemy : MonoBehaviour
 {
-    GameObject PlayerObj;                    //プレイヤーのオブジェクトの取得準備
-    private float m_speed = 0.005f;            //敵のスピードを1にする
-    private float m_playerToDis = 7.0f;     //プレイヤーが入ってきた際に追いかける距離
-    private Vector3 m_startPos;
+    public GameObject target;                   //GameObject型を変数targetで宣言します
+
     // Start is called before the first frame update
     void Start()
     {
-        PlayerObj = GameObject.FindWithTag("Player");   //プレイヤーのオブジェクトの取得
-        m_startPos = transform.position;                //座標の初期設定
+
     }
 
     // Update is called once per frame
     void Update()
     {
+            Quaternion m_lookRotation = Quaternion.LookRotation(target.transform.position - transform.position, Vector3.up);
 
-        //プレイヤーの位置から自分の位置を引いた数だけノーマライズする
-        Vector3 dir = (PlayerObj.transform.position - this.transform.position).normalized;
-        float   mag = (PlayerObj.transform.position - this.transform.position).magnitude;
+            m_lookRotation.z = 0;
+            m_lookRotation.x = 0;
 
-        //プレイヤーとの距離が指定よりも短くなったら
-        if(mag < m_playerToDis)
-        {
-            //プレイヤーに向かって移動する
-            Vector3 nowPos = transform.position;
-            dir.y = 0;
-            Vector3 move = dir.normalized * m_speed;
-            nowPos = nowPos + move;
-            transform.position = nowPos;
+            transform.rotation = Quaternion.Lerp(transform.rotation, m_lookRotation, 0.1f);     //
+            Vector3 m_speed = new Vector3(0f, 0f, 0.005f);                                      //ターゲットに向かって追跡する処理
 
-            //プレイヤーの方向を向く処理
-            float rot = Mathf.Atan2(dir.x, dir.z);
-            transform.rotation = Quaternion.AngleAxis(rot * Mathf.Rad2Deg, Vector3.up);
-        }
-        else
-        {
-        }
-        
-
+            transform.Translate(m_speed);
     }
 }
 
