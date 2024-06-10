4287469a6b7898fa1e2ceb8398b21070b4dddb99
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-06-10 13:42:27 +0900

    カウントダウン処理追加

--- SwordTrial/Assets/SotaFolder/Timer.cs
+++ SwordTrial/Assets/SotaFolder/Timer.cs
@@ -1,33 +1,88 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+//タイマー処理
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    private int m_countDownMinutes = 3;
-    private float m_countDownSeconds;
+    //カウントダウン分
+    [SerializeField] private int m_countDownMinutes = 3;
+    //カウントダウン秒
+    [SerializeField] private float m_countDownSeconds = 0;
+    //現在の時間を表示するテキスト
     private Text m_timerText;
 
-    // Start is called before the first frame update
     void Start()
     {
-        m_timerText = GetComponent<Text>();
-        m_countDownSeconds = m_countDownMinutes * 60;
+        m_timerText = GameObject.Find("TimerText").GetComponent<Text>();
+        //m_countDownSeconds = m_countDownMinutes * 60;
     }
 
-    // Update is called once per frame
     void Update()
     {
         //秒を減らす
+        CountDown();
+        
+        Debug.Log(m_countDownMinutes + "：" + m_countDownSeconds);
+
+
+
+        if (m_countDownMinutes <= 0 && m_countDownSeconds <= 0)
+        {
+            m_timerText.text = string.Format("00：00");
+        }
+        else 
+        {
+            m_timerText.text = string.Format(m_countDownMinutes + "：" + m_countDownSeconds);
+        }
         
 
+
         //タイマーがゼロになったら
-        if(m_countDownSeconds <= 0)
+        if (m_countDownSeconds <= 0)
         {
             //ゲームオーバーシーンに移行
 
         }
+    }
+
+    /// <summary>
+    /// カウントダウンの処理
+    /// </summary>
+    private void CountDown()
+    {
+        // タイムオーバーになると処理を止める
+        //if(m_countDownMinutes < 0 && m_countDownSeconds < 0)
+        //{
+        //    m_countDownMinutes = 0; 
+        //    m_countDownSeconds = 0;
+        //    return;
+        //}
+        //else
+        //{
+            
+        //}
+
+        //カウントを減らす
+        m_countDownSeconds -= Time.deltaTime;
+
+        //分を更新
+        if (m_countDownSeconds <= 0 && m_countDownMinutes >= 1)
+        {
+            m_countDownMinutes -= 1;
+            m_countDownSeconds = 1;
+        }
+    }
+
+    /// <summary>
+    /// int型に変換
+    /// </summary>
+    private int IntConvert(float floatNum)
+    {
+        int result = 0;
+
+        result = Mathf.FloorToInt(floatNum);
+
+        return result;
     }
 }
