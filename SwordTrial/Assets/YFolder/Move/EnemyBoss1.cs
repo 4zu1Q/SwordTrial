55ac6822a8eeb82c1dd25f93c6fbc60247e76d9f
itukikunmeuu <135081677+itukikunmeuu@users.noreply.github.com>
2024-06-11 18:59:06 +0900

    EnemyMoveの修正
    コーディング規約とコメントが少なかったので全体的にコメントを増やして、コーディング規約を守ったりした。自分のコメントの部分は終了。

--- SwordTrial/Assets/YFolder/Move/EnemyBoss1.cs
+++ SwordTrial/Assets/YFolder/Move/EnemyBoss1.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class EnemyBoss1 : MonoBehaviour
 {
-    private string m_targetObjectName;  //目標オブジェクトの名前
-    private float speed = 0.1f;         //スピード
-    GameObject m_targetObject;
+    private string targetObjectName;     //目標オブジェクトの名前
+    private float  speed = 0.1f;         //スピード
+    GameObject     targetObject;         //どのオブジェクトを基準にして動くかの処理
 
     // Start is called before the first frame update
     void Start()
     {
-        m_targetObjectName = "Player";
-        m_targetObject = GameObject.Find(m_targetObjectName);
+        targetObjectName = "Player";                        //基準となるターゲットのオブジェクト名の入力処理
+        targetObject = GameObject.Find(targetObjectName);   //オブジェクトの名前を入れる
     }
 
     // Update is called once per frame
     void Update()
     {
         //正規化
-        Vector3 dir = (m_targetObject.transform.position - this.transform.position).normalized;
+        Vector3 dir = (targetObject.transform.position - this.transform.position).normalized;
 
         //その方向に指定したスピード量で進む
         float vx = dir.x * speed;
         float vz = dir.z * speed;
-
+        //その座標分だけ進む処理
         this.transform.Translate(vx / 50, 0, vz / 50);
     }
 }
