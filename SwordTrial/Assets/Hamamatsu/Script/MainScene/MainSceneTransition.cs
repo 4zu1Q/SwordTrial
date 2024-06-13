330a22b1b6cdf9916606691e24fffac02ea81951
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-06-13 12:37:52 +0900

    メインシーンから勝利画面、敗北画面に遷移する処理を少しリファクタリングした

--- SwordTrial/Assets/Hamamatsu/Script/MainScene/MainSceneTransition.cs
+++ SwordTrial/Assets/Hamamatsu/Script/MainScene/MainSceneTransition.cs
@@ -1,70 +1,27 @@
 //メインシーンのシーン遷移
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainSceneTransition : MonoBehaviour
 {
-    public enum SelectNum
-    {
-        kRetry, //スタート
-        kOption,//説明
-        kSelect,   //終了
-    }
-
-    private bool m_isSelect;        //連続
-    private SelectNum m_selectNum;  //選択
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //変数初期化
-        m_isSelect = false;
-        m_selectNum = SelectNum.kRetry;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //スティックの入力値を格納
-        float input = Input.GetAxis("Vertical");
+        DebugSceneTransition();
+    }
 
-        if (input <= -0.5f && !m_isSelect)
-        {
-            m_isSelect = true;
-            m_selectNum = SelectNum.kSelect;
-            Debug.Log("Win");
-        }
-        else m_isSelect = false;
-
-
-
-        if (input >= 0.5f && !m_isSelect)
-        {
-            m_isSelect = true;
-            m_selectNum = SelectNum.kRetry;
-            Debug.Log("Lose");
-        }
-        else m_isSelect = false;
-
-
-        //長押しを回避するよう
-        if (input >= -0.1f && input <= 0.1f)
-        {
-            m_isSelect = false;
-        }
-
-
-
+    /// <summary>
+    /// デバッグ用シーン遷移
+    /// </summary>
+    private void DebugSceneTransition()
+    {
         //Aボタンを押したら
-        if (Input.GetButton("Abutton") && m_selectNum == SelectNum.kRetry)
+        if (Input.GetButton("Abutton"))
         {
             //ゲームシーンに移動
             SceneManager.LoadScene("WinScene");
         }
-        else if (Input.GetButton("Abutton") && m_selectNum == SelectNum.kSelect)
+        else if (Input.GetButton("Bbutton"))
         {
             //セレクトシーンに移行
             SceneManager.LoadScene("LoseScene");
