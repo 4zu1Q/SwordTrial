29248918eed6e1a93c197d56815873d4ea60c991
itukikunmeuu <135081677+itukikunmeuu@users.noreply.github.com>
2024-07-04 18:50:46 +0900

    up

--- SwordTrial/Assets/Yoshiyama/Script/Move/EnemyAnimation.cs
+++ SwordTrial/Assets/Yoshiyama/Script/Move/EnemyAnimation.cs
@@ -2,40 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAnimation : EnemyC
+public class EnemyAnimation : MonoBehaviour
 {
 
-    public string paramaterName = "";
+    private string paramaterName = "Attack1";
+
+    Animator m_anim;
+    bool m_pushFlag = false;
+
+    private void Start()
+    {
+        m_anim = GetComponent<Animator>();
+        //ランダム値の値に応じてアニメーションを切り替える
+        m_pushFlag = false;
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        //ランダム値の値に応じてアニメーションを切り替える
-        bool m_pushFlag = false;
+        
 
         // 通常攻撃
-        if (m_attackKinds == (int)AttackKinds.kNormalAttack)
+        if (Input.GetKeyDown("up"))
         {
             Debug.Log("通常攻撃");
-            m_pushFlag = true;
-        }
-        else if (m_attackKinds == (int)AttackKinds.kChargeAttack)
-        {
-            Debug.Log("溜め攻撃");
-            m_pushFlag = true;
+            if(m_pushFlag)
+            {
+                m_pushFlag = false;
+            }
+            else
+            {
+                m_pushFlag = true;
+            }
 
+            
         }
-        else if (m_attackKinds == (int)AttackKinds.kComboAttack)
-        {
-            Debug.Log("連続攻撃");
-            m_pushFlag = true;
-        }
-        else if (m_attackKinds == (int)AttackKinds.kRotateAttack)
-        {
-            Debug.Log("回転攻撃");
-            m_pushFlag = true;
-        }
+        //else if (Input.GetKeyDown("down"))
+        //{
+        //    Debug.Log("溜め攻撃");
+        //    m_pushFlag = true;
+        //}
+        //else if (Input.GetKeyDown("right"))
+        //{
+        //    Debug.Log("連続攻撃");
+        //    m_pushFlag = true;
+        //}
+        //else if (Input.GetKeyDown("left"))
+        //{
+        //    Debug.Log("回転攻撃");
+        //    m_pushFlag = true;
+        //}
+
         //パラメータの値を変更
-        Animator m_Animator = gameObject.GetComponent<Animator>();
-        m_Animator.SetBool(paramaterName, m_pushFlag); 
+
+        m_anim.SetBool(paramaterName, m_pushFlag);
+
+        Debug.Log(m_pushFlag);
     }
 }
