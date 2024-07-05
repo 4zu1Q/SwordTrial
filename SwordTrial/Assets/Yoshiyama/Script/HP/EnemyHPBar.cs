3de0065f83f0f9988f0309f61625a2afdaea23f8
itukikunmeuu <135081677+itukikunmeuu@users.noreply.github.com>
2024-07-05 21:54:09 +0900

    HPBarの設定
    HPBarを作りました。
    設定関係は行ってませんが、Barを左右に動かしたらいい感じに動きます。
    
    明日、スクリプトをいい感じに書いて連動します

---
+++ SwordTrial/Assets/Yoshiyama/Script/HP/EnemyHPBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHPBar : MonoBehaviour
+{
+    //HP表示用UI
+    [SerializeField] private GameObjrct HPUI;
+
+    //HP表示用スライダー
+    private Slider hpSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
