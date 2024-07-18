66d7ef8db49fef96a0950d61ff39734a4755154a
S0624 <mmasyumaro.012345@gmail.com>
2024-07-18 14:03:15 +0900

    リザルト仮完成
    まだ細かい調整は終わっておりません
    終わらせます

--- SwordTrial/Assets/Hamamatsu/Script/VictoryScene/TimerResult.cs
+++ SwordTrial/Assets/Hamamatsu/Script/VictoryScene/TimerResult.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 public class TimerResult : MonoBehaviour
 {
     //時間の計測結果を表示するテキスト
-    [SerializeField] private Text m_resultText;
+    [SerializeField] private Sprite[] m_timeNum;//数字の画像取得
+    [SerializeField] private Image[] m_timeImage;//表示する時間の画像取得
 
     void Start()
     {
@@ -16,7 +17,6 @@ public class TimerResult : MonoBehaviour
     void Update()
     {
         TimeResultView();
-        //Debug.Log(Timer.m_countDownMinutes + "：" + Timer.m_countIntSeconds);
     }
 
     /// <summary>
@@ -24,7 +24,30 @@ public class TimerResult : MonoBehaviour
     /// </summary>
     private void TimeResultView()
     {
-        Debug.Log(Timer.m_countIntSeconds);
-        m_resultText.text = "Time:" + Timer.m_countDownMinutes + ":" + Timer.m_countIntSeconds;
+        // 残り時間が10秒より多ければ
+        if(Timer.m_countIntSeconds >= 10)
+        {
+            var num1 = Timer.m_countIntSeconds / 10;
+            var num2 = Timer.m_countIntSeconds - num1 * 10;
+            // 時間の画像変更
+            TimeImageChenge(num1, num2);
+        }
+        //それ以外だったら
+        else
+        {
+            // 時間の画像変更
+            TimeImageChenge(0, Timer.m_countIntSeconds);
+        }
+    }
+    /// <summary>
+    ///  時間の画像の変更処理
+    /// </summary>
+    /// <param name="num1">秒数の10の位の数値</param>
+    /// <param name="num2">秒数の1の位の数値</param>
+    private void TimeImageChenge(int num1, int num2)
+    {
+        m_timeImage[0].sprite = m_timeNum[Timer.m_countDownMinutes];
+        m_timeImage[1].sprite = m_timeNum[num1];
+        m_timeImage[2].sprite = m_timeNum[num2];
     }
 }
